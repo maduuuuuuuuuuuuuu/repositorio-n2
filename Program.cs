b447d5c64b0aae8f7e69e244763763e40dc8a48b
madu <maduamaral2006@gmail.com>
2023-06-18 14:30:43 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string senha;
+Console.WriteLine("ola! digite a senha");
+senha = Console.ReadLine()!;
+if(senha == "1234abcd"){
+    Console.WriteLine("acesso permitido");
+}
+else{
+    Console.WriteLine("acesso negado");
+}
